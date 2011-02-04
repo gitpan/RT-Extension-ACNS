2ce0abe2997618ef5aab54c2a4d35ee068ba1b4d
Руслан У. Закиров <Ruslan.Zakirov@gmail.com>
2011-02-04 00:32:52 +0000

    Import of RUZ/RT-Extension-ACNS-0.01 from CPAN.
    gitpan-cpan-distribution: RT-Extension-ACNS
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         RUZ/RT-Extension-ACNS-0.01.tar.gz
    gitpan-cpan-author:       RUZ
    gitpan-cpan-maturity:     released

---
+++ lib/RT/Extension/ACNS.pm
@@ -0,0 +1,59 @@
+use 5.008003;
+use strict;
+use warnings;
+
+package RT::Extension::ACNS;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT::Extension::ACNS - parse ACNS messages and extract info into custom fields
+
+=head1 DESCRIPTION
+
+ACNS stands for Automated Copyright Notice System. It's an open source,
+royalty free system that universities, ISP's, or anyone that handles large
+volumes of copyright notices can implement on their network to increase
+the efficiency and reduce the costs of responding to the notices... 
+See "http://mpto.unistudios.com/xml/" for more details.
+
+This extension for RT is a configurable scrip that parses ACNS XML from
+incomming messages and stores it in custom fields.
+
+=head1 INSTALLATION
+
+    perl Makefile.PL
+    make
+    make install
+    make initdb
+
+In F<RT_SiteConfig.pm>:
+
+    Set(@Plugins, qw(
+        RT::Extension::ACNS
+        ... other plugins ...
+    ));
+    Set( %ACNS,
+        ... configuration ...
+    );
+
+=head1 CONFIGURATION
+
+The scrip is configured thorugh C<%ACNS> config option that described in
+details in F<etc/RT_ACNSConfig.pm>.
+
+=cut
+
+
+=head1 AUTHOR
+
+Ruslan Zakirov E<lt>ruz@bestpractical.comE<gt>
+
+=head1 LICENSE
+
+Under the same terms as perl itself.
+
+=cut
+
+1;
